64d6f53fe106ffcd6bf90bea484a08db777d11f7
jorgevillaverde <jorgevillaverde@gmail.com>
2020-11-11 15:54:54 -0300

    fix: use standar web socket instead of SockJS

--- src/plugin/spa-reloader-plugin.js
+++ src/plugin/spa-reloader-plugin.js
@@ -1,14 +1,57 @@
-
 const methods = {
 
-    $websocketConnection() {
-        return {
-            "created_at": "2020-11-10T11:08:47.073626-03:00",
-            "namespace": "toucan",
-            "name": "spa",
-            "image": "nginx:1.14.1",
-            "sha256": "32fdf92b4e986e109e4db0865758020cb0c3b70d6ba80d02fe87bad5cc3dc228"
+    $webSocketsConnect (options) {
+        if(!options) {
+            return null
         }
+        console.log("Connecting to SPA Relaoad at: ", options.spaReloaderURL);
+        let ws = new WebSocket(options.spaReloaderURL);
+        ws.onopen = () => {
+            console.log("Connected to SPA Relaoad at: ", options.spaReloaderURL);
+            // Restart reconnect interval
+            options.reconnectInterval = options.reconnectInterval || 1000;
+        }
+
+        ws.onmessage = (event) => {
+            // New message from the backend - use JSON.parse(event.data)
+            methods.handleNotification(event);
+        }
+
+        ws.onclose = (event) => {
+            console.log("Closing Connection to SPA Relaoad at: ", options.spaReloaderURL);
+            if (event) {
+                // Event.code 1000 is our normal close event
+                if (event.code !== 1000) {
+                    let maxReconnectInterval = options.maxReconnectInterval || 3000;
+                    setTimeout(() => {
+                        if (options.reconnectInterval < maxReconnectInterval) {
+                            // Reconnect interval can't be > x seconds
+                            options.reconnectInterval += 1000;
+                        }
+                        methods.$webSocketsConnect();
+                    }, options.reconnectInterval);
+                }
+            }
+        }
+
+        ws.onerror = (error) => {
+            console.log(error);
+            ws.close();
+        }
+        return ws
+    },
+
+    /*
+     * Handle notifications
+     */
+    handleNotification (event) {
+        console.log(event);
+        //options.store.dispatch('notifications/setNotifications', params.data)
+    },
+
+    $webSocketsDisconnect() {
+        // Our custom disconnect event
+        //ws.close();
     },
 
     $hashChanged(oldHash, newHash) {
@@ -21,50 +64,47 @@ const methods = {
 }
 
 const DEFAULTS = {
-    initHash : ''
+    spaReloaderURL : 'localhost:8081',
+    initHash : '',
+    reconnectInterval: 1000
 };
 
 export default {
 
-    // called by Vue.use(SecondPlugin)
+    /**
+     * Called by Vue.use to install the plugin
+     * @param {*} Vue instance of Vue
+     * @param {*} options options of the plugin
+     *   - spaReloaderURL: URL of the websocket server
+     */
     install(Vue, options = {}) {
+        console.log("Plugin Options: ", options)
 
         const {
-            SockJS,
-            Stomp,
+            spaReloaderURL
         } = options
 
-        // throw errors
-        if (!SockJS) throw new Error('Please, specify in the settings your sockjs-client package')
-        if (!Stomp) throw new Error('Please, specify in the settings your webstomp-client package.')
-
-        let currentHash = options.initHash || DEFAULTS.initHash
-        let isHashNew = false;
+        // Validate options
+        if (!spaReloaderURL) throw new Error('Invalid SPA Reloader URL, please supply the URL where SPA Reloader is running.')
 
         // create a mixin
         Vue.mixin({
             created() {
-                console.log('user options', options.initHash);
+                // console.log('user options', options.initHash);
                 console.log('default options', DEFAULTS.initHash);
-                console.log('selected options', currentHash);
+                // console.log('selected options', currentHash);
             },
 
             mounted() {
                 // call method to star listening to websocket
                 console.log('mounted. aca me quedo escuchando el websocket');
-                let response = methods.$websocketConnection()
-                isHashNew = methods.$hashChanged(currentHash, response.sha256)
-                if (isHashNew) {
-                    currentHash = response.sha256;
-                    console.log('hash has changed!')
-                }
+                methods.$webSocketsConnect(options);
+                // isHashNew = methods.$hashChanged(currentHash, response.sha256)
+                // if (isHashNew) {
+                //     currentHash = response.sha256;
+                //     console.log('hash has changed!')
+                // }
             }
-
         });
-
-        // instance methods
-        Vue.prototype.$helloMethod = function() {
-            console.log('hello from prototype method');
-        }
-    }
+    },
 }
