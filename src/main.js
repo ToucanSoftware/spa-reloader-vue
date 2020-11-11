e3a95b0ac11df05010d2147ae7f21f2e00b73eb8
Fernanda Fernandez <ffernandamarisol@gmail.com>
2020-11-11 10:22:13 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import SpaReloaderPlugin from './plugin/spa-reloader-plugin'
+import SockJS from 'sockjs-client';
+import Stomp from 'webstomp-client';
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+Vue.use(SpaReloaderPlugin, {SockJS, Stomp, initHash: '246810'})
+
 
 new Vue({
-  store,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
+
